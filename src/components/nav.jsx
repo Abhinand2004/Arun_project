1e168c78ec5cea0df7a13f4963669d284cf2984b
abhinand <abhinandc293@gmail.com>
2025-10-07 10:02:08 +0530

    second

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-scroll";
-import "./Nav.css";
+import "./nav.css";
 
 function Nav() {
   const [open, setOpen] = useState(false);
